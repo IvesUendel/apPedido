59235f995aa9fbaffcb21aaadb23d929c2b28fe3
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-11-11 13:07:07 -0300

    Start creating the project using SpringBot

---
+++ src/main/java/br/com/apPedido/ApPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apPedido/ApPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

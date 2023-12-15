a9867c3e2e120e0da0fa7290c098a22a51795abb
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-12-15 19:57:57 -0300

    Pequena adição da anotação RequestBody no método incluir

--- src/main/java/br/com/apPedido/controller/LegalCustomerController.java
+++ src/main/java/br/com/apPedido/controller/LegalCustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class LegalCustomerController {
 	}
 	
 	@PostMapping(value = "/include")
-	public void includeData(LegalCustomer legalCustomer) {
+	public void includeData(@RequestBody LegalCustomer legalCustomer) {
 		legalCustomerService.includeData(legalCustomer);
 	}
 	
